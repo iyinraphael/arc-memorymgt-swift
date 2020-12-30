4768d4dcdb7aab951007ce941473255cd7f6ae6b
iyinraphael <iyinraphael@gmail.com>
2020-12-30 14:22:49 +0100

    brief common problems of capturing

--- capture-list.playground/Pages/common-problems.xcplaygroundpage/Contents.swift
+++ capture-list.playground/Pages/common-problems.xcplaygroundpage/Contents.swift
@@ -69,4 +69,33 @@ print("Done!!")
  Now, in real project code it’s rare to find strong reference cycles that are so obvious, but that just means it’s all the more important to use weak capturing to avoid the problem entirely.
  */
 
+// MARK: - Accidental strong references
+//Swift won’t let you use implicit self inside closures, which helps reduce a common type of retain cycle.
+
+// MARK: - Copeis of closures
+var numberOfLinesLogged = 0
+
+let logger1 = {
+    numberOfLinesLogged += 1
+    print("Lines logged: \(numberOfLinesLogged)")
+}
+
+logger1()
+
+let logger2 = logger1
+logger2()
+logger1()
+logger2()
+
+/**
+ When to use strong, when to use weak, when to use unowned
+ Now that you understand how everything works, let’s try to summarize whether to use strong, weak, or unowned references:
+
+ 1. If you know for sure your captured value will never go away while the closure has any chance of being called, you can use unowned. This is really only for the handful of times when weak would cause annoyances to use, but even when you could use guard let inside the closure with a weakly captured variable.
+ 
+ 2. If you have a strong reference cycle situation – where thing A owns thing B and thing B owns thing A – then one of the two should use weak capturing. This should usually be whichever of the two will be destroyed first, so if view controller A presents view controller B, view controller B might hold a weak reference back to A.
+ 
+ 3. If there’s no chance of a strong reference cycle you can use strong capturing. For example, performing animation won’t cause self to be retained inside the animation closure, so you can use strong capturing.
+ If you’re not sure which to use, start out with weak and change only if you need to.
+ */
 //: [Next](@next)
